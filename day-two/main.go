4386b8f4933240cb1981c328def8adebdf2a5973
Chwoeger Charleston <122495108+shwaygrr@users.noreply.github.com>
2024-12-03 11:43:48 -0500

    day 2 part 2 solution

--- day-two/main.go
+++ day-two/main.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 )
 
-func convertDataToArr(file_name string) [1000][]int {
-	var data_arr [1000][]int
+func convertDataToreport(file_name string) [1000][]int {
+	var data_report [1000][]int
 
 	data, err := os.ReadFile(file_name)
 
@@ -28,28 +28,27 @@ func convertDataToArr(file_name string) [1000][]int {
 				panic(err)
 			}
 
-			data_arr[i] = append(data_arr[i], level_int)
+			data_report[i] = append(data_report[i], level_int)
 		}
 	}
 
-	return data_arr
+	return data_report
 }
 
-func isSafe(levels []int) bool {
-
-	is_increasing := levels[0]-levels[1] < 0 //assumption
+func isSafe(report []int) bool {
+	is_increasing := report[0]-report[1] < 0 //assumption
 
 	if is_increasing {
-		for i := 0; i < len(levels)-1; i++ {
-			diff := levels[i] - levels[i+1]
+		for i := 0; i < len(report)-1; i++ {
+			diff := report[i] - report[i+1]
 
 			if diff > -1 || diff < -3 {
 				return false
 			}
 		}
 	} else {
-		for i := 0; i < len(levels)-1; i++ {
-			diff := levels[i] - levels[i+1]
+		for i := 0; i < len(report)-1; i++ {
+			diff := report[i] - report[i+1]
 
 			if diff < 1 || diff > 3 {
 				return false
@@ -72,12 +71,64 @@ func countSafeReports(reports [1000][]int) int {
 	return count
 }
 
+func isSafeWithDampener(report []int, assume_increasing bool) bool {
+	i := 0
+	j := 1
+	dampened_index := 80 // guarenteed number thats not an index in the input
+
+	for j < len(report) {
+		//assume increasing
+		diff := report[i] - report[j]
+
+		var invalid_range bool
+
+		if assume_increasing {
+			invalid_range = diff > -1 || diff < -3
+		} else {
+			invalid_range = diff < 1 || diff > 3
+		}
+
+		if invalid_range {
+			if dampened_index == 80 { //means first time dampening
+				dampened_index = j //track skipped index
+				j++                //skip
+				// fmt.Println("index skipped for level", report)
+			} else {
+				return false
+			}
+		} else {
+			if dampened_index == i+1 {
+				dampened_index = 81 //prevent triggering another dampening
+				i += 2
+			} else {
+				i++
+			}
+			j++
+		}
+	}
+	return true
+}
+
+func countIsSafeWithDampener(reports [1000][]int) int {
+	count := 0
+
+	for _, report := range reports {
+		if isSafe(report) || isSafe(report[1:]) || isSafeWithDampener(report, true) || isSafeWithDampener(report, false) {
+			count++
+		}
+	}
+
+	return count
+}
+
 func main() {
-	reports := convertDataToArr("input.txt")
+	reports := convertDataToreport("input.txt")
 
 	// part 1
 	safe_report_count := countSafeReports(reports)
 	fmt.Println("Safe Report Count:", safe_report_count)
 
 	// part 2
+	safe_reports_with_dampener_count := countIsSafeWithDampener(reports)
+	fmt.Println("Safe Report Count:", safe_reports_with_dampener_count)
 }
